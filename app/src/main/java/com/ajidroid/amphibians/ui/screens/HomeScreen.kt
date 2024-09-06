c511a4954c898c16ccc5e272406b22a542d06629
Ajit Upadhyay <ajitbaba1003@gmail.com>
2024-09-06 06:32:41 +0530

    Building AsyncImage

--- app/src/main/java/com/ajidroid/amphibians/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/ajidroid/amphibians/ui/screens/HomeScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -41,6 +42,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.ajidroid.amphibians.R
 import com.ajidroid.amphibians.model.Amphibian
 
@@ -178,7 +180,10 @@ fun AmphibianCard(
             Spacer(modifier = Modifier.height(8.dp))
 
             AsyncImage(
-                model = amphibian.image,
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data(amphibian.image)
+                    .crossfade(true)
+                    .build(),
                 modifier = Modifier.fillMaxWidth(),
                 placeholder = painterResource(id = R.drawable.loading_img),
                 error = painterResource(id = R.drawable.ic_broken_image),
